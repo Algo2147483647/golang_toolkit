1c24a0f29bc43e5e92d22f2250503cfe07170574
Algo2147483647 <nullllllllun@gmail.com>
2026-01-28 00:50:53 +0800

    init

--- rule_engine/node.go
+++ rule_engine/node.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type NodeIf interface {
 	GetType() string
+	Evaluate() (result ValueIf, err error)
 }
 
 const (
@@ -11,22 +12,41 @@ const (
 	NodeTypeValue = "value_node"
 )
 
-func Calculate(node NodeIf) (result ValueIf, err error) {
-	switch node.GetType() {
+// ExprNode is the base structure for expression nodes in the rule engine.
+type NodeBase struct {
+	Type         string
+	Value        ValueIf                `json:"value"`
+	Operator     OperatorIf             `json:"operator"`
+	Params       map[string]interface{} `json:"params"`
+	PreNodeList  []NodeIf               `json:"pre_node_list"`
+	PostNodeList []NodeIf               `json:"post_node_list"`
+}
+
+func (n *NodeBase) GetType() string {
+	return n.Type
+}
+
+// Evaluate evaluates the expression node with given context (variables)
+func (n *NodeBase) Evaluate() (result ValueIf, err error) {
+	switch n.GetType() {
 	case NodeTypeExpr:
-		return node.(*ExprNode).Evaluate()
+		return n.Value, nil
 
 	case NodeTypeValue:
-		return node.(*ValueNode).Value, nil
+		nodeListResult := make([]ValueIf, 0, len(n.PostNodeList))
+
+		for _, node := range n.PostNodeList {
+			nodeResult, err := node.Evaluate()
+			if err != nil {
+				return nil, err
+			}
+
+			nodeListResult = append(nodeListResult, nodeResult)
+		}
+
+		return n.Operator.Evaluate(nodeListResult...)
+
+	default:
+		return nil, fmt.Errorf("unknown node type: %s", n.GetType())
 	}
-
-	return nil, fmt.Errorf("unknown node type: %s", node.GetType())
-}
-
-type ValueNode struct {
-	Value ValueIf `json:"value"`
-}
-
-func (n *ValueNode) GetType() string {
-	return NodeTypeValue
 }
