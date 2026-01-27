5d6b9276b11b4d8651ceb02ecec5f1020b7bfe38
Algo2147483647 <nullllllllun@gmail.com>
2026-01-28 02:12:15 +0800

    init

--- event_model/action.go
+++ event_model/action.go
@@ -7,8 +7,13 @@ type Action interface {
 
 type ActionBase struct {
 	Attributes []string `json:"attributes"`
+	Params     map[string]interface{}
 }
 
 func (c *ActionBase) GetAttributes() []string {
 	return c.Attributes
+}
+
+func (c *ActionBase) Execute() error {
+	return nil
 }
