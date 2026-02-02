8f45a531165a65a102f3db7d097402c57fb58d88
Algo2147483647 <nullllllllun@gmail.com>
2026-02-03 01:17:54 +0800

    init

--- event_model/unit.go
+++ event_model/unit.go
@@ -1,13 +1,15 @@
 package event_model
 
 import (
+	"context"
 	"github.com/Algo2147483647/golang_toolkit/event_model/eca"
 	"github.com/Algo2147483647/golang_toolkit/math/graph"
 )
 
 type Unit struct {
 	eca.ECA
-	Key    string                 `json:"key"`
+	Key    string `json:"key"`
+	State  string
 	Pre    []*Unit                `json:"pre"`
 	Post   []*Unit                `json:"post"`
 	Params map[string]interface{} `json:"params"`
@@ -41,3 +43,47 @@ const (
 	UnitStateConditionPass = "condition_passed"
 	UnitStateCompleted     = "completed"
 )
+
+func Run(ctx context.Context, units []*Unit, eventTypes []string, req interface{}) error {
+	// 1. Get units by events
+	newUnits := make([]*Unit, 0)
+
+	for _, unit := range units {
+		for _, eventType := range eventTypes {
+			if unit.State == UnitStateInProgress && unit.EventTrigger(ctx, eventType) {
+				newUnits = append(newUnits, unit)
+			}
+		}
+	}
+
+	units = newUnits
+
+	// 2. Run units
+	for _, unit := range units {
+		if !unit.ConditionPass(ctx, req) {
+			continue
+		}
+
+		unit.State = UnitStateConditionPass
+
+		if err := unit.ActionExecute(ctx); err != nil {
+			return err
+		}
+
+		unit.State = UnitStateCompleted
+
+		for _, item := range unit.Post {
+			if item.State == UnitStateNotStarted {
+				item.State = UnitStateInProgress
+			}
+
+			for _, eventType := range eventTypes {
+				if item.State == UnitStateInProgress && item.EventTrigger(ctx, eventType) {
+					newUnits = append(newUnits, item)
+				}
+			}
+		}
+	}
+
+	return nil
+}
